ca2bbee21ac76c861edd921c95f1ed2f47a94eee
Jose Jordan <mundodigital.pro@gmail.com>
2024-05-08 11:52:45 +0200

    new share button functionality sharing text with other apps

--- app/src/main/java/com/example/mlvisionkotlin/MainActivity.kt
+++ app/src/main/java/com/example/mlvisionkotlin/MainActivity.kt
@@ -39,28 +39,31 @@ class MainActivity : AppCompatActivity() {
         initializeCameraCaptureLauncher() // Inicializa el lanzador de captura de cámara
         setupReadTextButtonListener() // Configura el listener del botón para leer texto
         setupCaptureImageButtonListener() // Configura el listener del botón para capturar imágenes
+        setupShareButtonListener() //Configura el listener del botón para compartir texto
     }
 
     // Inicializa el lanzador para seleccionar imágenes desde el almacenamiento del dispositivo
     private fun initializeImagePickerLauncher() {
-        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-            handleImagePickerResult(uri)
-        }
+        imagePickerLauncher =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+                handleImagePickerResult(uri)
+            }
     }
 
     // Inicializa el lanzador para capturar imágenes con la cámara
     private fun initializeCameraCaptureLauncher() {
-        cameraCaptureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
-            if (success) {
-                imageUri?.let { uri ->
-                    binding.imageViewId.setImageURI(uri) // Muestra la imagen capturada
-                    recognizeTextFromImage(uri) // Inicia el reconocimiento de texto en la imagen
-                }
-            } else {
-                showToast("Image capture failed. Please try again.") // Muestra error si la captura falla
+        cameraCaptureLauncher =
+            registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+                if (success) {
+                    imageUri?.let { uri ->
+                        binding.imageViewId.setImageURI(uri) // Muestra la imagen capturada
+                        recognizeTextFromImage(uri) // Inicia el reconocimiento de texto en la imagen
+                    }
+                } else {
+                    showToast("Image capture failed. Please try again.") // Muestra error si la captura falla
 
+                }
             }
-        }
     }
 
     // Configura el listener para el botón de lectura de texto
@@ -77,17 +80,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Configura el listener para el boton de compartir texto
+    private fun setupShareButtonListener() {
+        binding.shareButtonId.setOnClickListener {
+            launchShare()
+        }
+    }
+
+    //Lanza la funcion compartir texto
+    private fun launchShare() {
+        val textToShare = binding.resultText.text.toString()
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, textToShare)
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TITLE, "Text to Share")
+        }
+        val shareIntent = Intent.createChooser(intent, "Share Text")
+        startActivity(shareIntent)
+    }
+
     // Verifica el permiso de cámara y, si está concedido, lanza la cámara; de lo contrario, solicita el permiso
     private fun checkCameraPermissionAndLaunchCamera() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
         } else {
             launchCamera() // Lanza la cámara si el permiso ya está concedido
         }
     }
 
     // Maneja el resultado de la solicitud de permisos
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -127,7 +162,8 @@ class MainActivity : AppCompatActivity() {
     // Crea un archivo de imagen en el almacenamiento externo privado
     @Throws(IOException::class)
     private fun createImageFile(): File {
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val timeStamp: String =
+            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
             "JPEG_${timeStamp}_",
@@ -141,7 +177,6 @@ class MainActivity : AppCompatActivity() {
     // Maneja el resultado de la selección de imágenes
     private fun handleImagePickerResult(uri: Uri?) {
         uri ?: return showToast("Image selection failed. Please try again.")
-
         imageUri = uri
         binding.imageViewId.setImageURI(uri) // Muestra la imagen seleccionada
         recognizeTextFromImage(uri) // Inicia el reconocimiento de texto en la imagen seleccionada
@@ -182,6 +217,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 101 // Código de solicitud de permiso de cámara
+        private const val CAMERA_PERMISSION_REQUEST_CODE =
+            101 // Código de solicitud de permiso de cámara
     }
 }
